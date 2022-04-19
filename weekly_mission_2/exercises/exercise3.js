18beeeac3abb28e684e3b958d2411143d4ea1c78
Fernando Cortez <fernandolcortezlgarcia@gmail.com>
2022-04-18 19:43:08 -0500

    agregando ejercicios3

---
+++ weekly_mission_2/exercises/exercise3.js
@@ -0,0 +1,22 @@
+//Tomando de referencia el ejercicio 1, convierte esos objetos a definiciones de clases, trata de aplicar lo visto en los ejemplos.
+const explorer = {
+    name: "Explorer 1",
+    exercises_completed: 10,
+    rate: 99,
+    city: "CDMX",
+    stack: ["js", "C#"]
+}
+const mission = {
+    mission1: "Onboarding",
+    mission2: "Frontend",
+}
+const mission1 = {
+    name: "Onboarding",
+    isFinished: true,
+    exercisesFinished: true
+}
+const mission2 = {
+    name: "Frontend",
+    isFinished: true,
+    exercisesFinished: true
+}
